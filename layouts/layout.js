c1bb075e390166425176859516c4748979b543d5
ysjiang4869 <1259012537@qq.com>
2021-06-30 10:53:31 +0800

    use avatar instead of gavatar

--- layouts/layout.js
+++ layouts/layout.js
@@ -42,7 +42,7 @@ const Layout = ({
                   alt={BLOG.author}
                   width={24}
                   height={24}
-                  src={`https://gravatar.com/avatar/${emailHash}`}
+                  src="/avatar.svg"
                   className="rounded-full"
                 />
                 <p className="ml-2 md:block">{BLOG.author}</p>
